a6de0cdc09ddfefcf508bc762b05daf3eee14bae
SlowRiot <slowriot@voxelstorm.com>
2018-02-14 22:54:45 +0000

    auto-increment version.h in project

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
-	static const long BUILD  = 0;
-	static const long REVISION  = 0;
+	static const long BUILD  = 1;
+	static const long REVISION  = 10;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 6;
-	#define RC_FILEVERSION 1,0,0,0
-	#define RC_FILEVERSION_STRING "1, 0, 0, 0\0"
-	static const char FULLVERSION_STRING [] = "1.0.0.0";
+	static const long BUILDS_COUNT  = 9;
+	#define RC_FILEVERSION 1,0,1,10
+	#define RC_FILEVERSION_STRING "1, 0, 1, 10\0"
+	static const char FULLVERSION_STRING [] = "1.0.1.10";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 0;
+	static const long BUILD_HISTORY  = 1;
 	
 
 }

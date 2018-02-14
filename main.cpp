8f3a6dae99bc3fcfd7100de94258a80714250ff2
SlowRiot <slowriot@voxelstorm.com>
2018-02-14 23:43:05 +0000

    add a simple config file processing library and load bot token from config file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <args.hxx>
+#include <INIReader.h>
 #include "version.h"
 
 auto main(int argc, char *argv[])->int {
@@ -12,11 +13,16 @@ auto main(int argc, char *argv[])->int {
     return version;
   };
 
+  std::string config_filename{"telegramtrackerbot.conf"};                       // default config file name
+  std::string const token_default{"123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"}; // telegram's official example token
+  std::string token{token_default};
+
   args::ArgumentParser arg_parser("Telegram Tracker Bot acts as an interface between Telegram and a GPS tracker unit.", get_version());
   args::HelpFlag help(arg_parser, "help", "Display this help information.", {'h', "help"});
   args::Flag version(arg_parser, "version", "Show version information and exit.", {"version"});
   args::Flag verbose(arg_parser, "verbose", "Show verbose output.", {'v', "verbose"});
   args::ValueFlag<std::string> flag_token(arg_parser, "token", "The token the bot should use to communicate with Telegram.", {'t', "token"});
+  args::ValueFlag<std::string> flag_config(arg_parser, "config", "Load a configuration from a config file other than the default " + config_filename + ".", {'c', "config"});
 
   try {
     arg_parser.ParseCLI(argc, argv);
@@ -25,17 +31,36 @@ auto main(int argc, char *argv[])->int {
       std::cout << get_version() << std::endl;
       return EXIT_SUCCESS;
     }
-
-    std::string token;
-    if(flag_token) {
-      token = args::get(flag_token);
-    } else {
-      std::cout << "Please supply a bot token on the commandline using --token." << std::endl;
-      return EXIT_FAILURE;
+    if(flag_config) {
+      config_filename = args::get(flag_config);                                 // read the config file option before trying to load a config file
     }
-
     if(verbose) {
-      std::cout << "Using bot token " << token << std::endl;
+      std::cout << "Reading configuration from " << config_filename << "..." << std::endl;
+    }
+    INIReader config_file(config_filename);
+    if(config_file.ParseError() >= 0) {
+      token = config_file.Get("", "token", token);
+      if(verbose) {
+        std::cout << "Successfully loaded configuration from " << config_filename << "." << std::endl;
+      }
+    } else {
+      if(verbose) {
+        std::cout << "Failed to load " << config_filename << "." << std::endl;
+      }
+    }
+    if(flag_token) {
+      token = args::get(flag_token);                                            // if we've provided a token to overwrite the original, replace it here
+    }
+    if(verbose) {
+      std::cout << "Using bot token" << token;
+      if(token == token_default) {
+        std::cout << " (this is the default token, and must be changed)";
+      }
+      std::cout << "." << std::endl;
+    }
+    if(token.empty() || token == token_default) {
+      std::cerr << "Please supply a valid bot token in the configuration file " << config_filename << ", or on the commandline using --token." << std::endl;
+      return EXIT_FAILURE;
     }
 
     // TODO
--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
-	static const long BUILD  = 1;
-	static const long REVISION  = 10;
+	static const long BUILD  = 9;
+	static const long REVISION  = 68;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 9;
-	#define RC_FILEVERSION 1,0,1,10
-	#define RC_FILEVERSION_STRING "1, 0, 1, 10\0"
-	static const char FULLVERSION_STRING [] = "1.0.1.10";
+	static const long BUILDS_COUNT  = 26;
+	#define RC_FILEVERSION 1,0,9,68
+	#define RC_FILEVERSION_STRING "1, 0, 9, 68\0"
+	static const char FULLVERSION_STRING [] = "1.0.9.68";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 1;
+	static const long BUILD_HISTORY  = 9;
 	
 
 }
